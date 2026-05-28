3fcef2e82d8715b28e211aecb5aa3e77d7378511
Thiago <thiago@ISC.local>
2026-05-28 16:01:50 -0300

    Ajuste: troca do nome das rotas

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -7,15 +7,15 @@ import ClientesAttController from "../controllers/ClientesAttController.js";
 const routers = Router();
 
 //faz download do arquivo ncm atualizado
-routers.get("/download/ncm", DownloadNcmListController);
+routers.get("/download/ncmlist", DownloadNcmListController);
 
 //faz a busca do ncm e retorna o resultado, valido ou vencido
 routers.post("/search/ncm", SearchNcmController);
 
 //consulta no banco de dados nexus compara com ncmData e retorna ncms invalidos
-routers.get("/search/ncmlist", SearchSendNcmController);
+routers.get("/download/ncmclient", SearchSendNcmController);
 
 //consulta api externa netbird e retorna info dos clientes
-routers.get("/clientes", ClientesAttController);
+routers.get("/clients", ClientesAttController);
 
 export default routers;
